459ec193401949a68bb06a9d750e96388a0998eb
opzpy123 <1441120973@qq.com>
2021-09-01 09:20:38 +0800

    1.1

--- src/main/java/com/opzpy123/Opzpy123Application.java
+++ src/main/java/com/opzpy123/Opzpy123Application.java
@@ -1,9 +1,11 @@
 package com.opzpy123;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.opzpy123.mapper")
 public class Opzpy123Application {
 
     public static void main(String[] args) {
