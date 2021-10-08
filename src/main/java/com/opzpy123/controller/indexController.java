d0ec1bb05ef13125b835f3275782ca9adc287f85
opzpy123 <1441120973@qq.com>
2021-10-08 16:10:16 +0800

    注册登录log信息

--- src/main/java/com/opzpy123/controller/indexController.java
+++ src/main/java/com/opzpy123/controller/indexController.java
@@ -2,6 +2,7 @@ package com.opzpy123.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
